b3851186407930a9802e8b82aa0eb606f6dc76f9
Shubham22Bisht <shubhambisht1924@gmail.com>
2024-07-01 13:11:06 +0530

    added env file

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/Users.js";
-const PORT=3000;
-const app=express();
+import dotenv from "dotenv";
+dotenv.config();
 
+const PORT = 3000;
+const DATABASE_URI = process.env.MONGODB_URI;
+const app = express();
 app.use(express.json());
 app.use(cors());
 
 // authentication route
-app.use("/auth",userRouter);
+app.use("/auth", userRouter);
+mongoose
+  .connect(DATABASE_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
 
-mongoose.connect("mongodb+srv://shubhambisht1924:H2t02PtDRlOH4D1j@cluster0.axbayjz.mongodb.net/Expense_Tracker");
-
-app.get("/",(req,res)=>{
-    res.send("Hello there");
-})
-
-app.listen(PORT,()=>{
-    console.log("Server Started");
-})
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
